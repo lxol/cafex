16d491a46f7988fa852ce2cae500958ac97f24f5
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-04-17 15:47:02 +0100

    Add isHot/isFood properties

--- src/main/scala/cafex/Checkout.scala
+++ src/main/scala/cafex/Checkout.scala
@@ -8,6 +8,8 @@ class Checkout {
   sealed trait Item {
     val name: String
     val price: BigDecimal
+    val isFood: Boolean
+    val isHot: Boolean
   }
 
   object Item {
@@ -15,21 +17,29 @@ class Checkout {
     private object Cola extends Item {
       override val name: String =  "Cola"
       override val price = BigDecimal("0.5")
+      override val isFood: Boolean = false
+      override val isHot: Boolean = false
     }
 
     private object Coffee extends Item {
       override val name: String = "Coffee"
       override val price: BigDecimal = BigDecimal("1.0")
+      override val isFood: Boolean = false
+      override val isHot: Boolean = true
     }
 
     private object CheeseSandwich extends Item {
       override val name: String = "Cheese Sandwich"
       override val price: BigDecimal =  BigDecimal("2.0")
+      override val isFood: Boolean = true
+      override val isHot: Boolean = false
     }
 
     private object SteakSandwich extends Item {
       override val name: String = "Steak Sandwich"
       override val price: BigDecimal = BigDecimal("4.5")
+      override val isFood: Boolean = true
+      override val isHot: Boolean = true
     }
 
     def maybeGetItem(str: String) = str match {
